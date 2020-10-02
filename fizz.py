48666d57d333b1fb547f53a24fc9fa1d883319fa
Csaba Sipocz <glinto@gmail.com>
2020-10-02 04:47:46 +0200

    Update fizz.py

--- fizz.py
+++ fizz.py
@@ -1,5 +1,5 @@
 # Write a program in Python to accept two integers. The first one is the starting integer. 
 # The second is the ending integer (inclusive). Run through the specified sequence 
 # of integer numbers. If the number is divisible by 3, print "Fizz". If the number 
-# is divisible by 5, print "Buzz", otherwise print the integer being tested. 
-# The results for each integer tested should be on its own line.
+# is divisible by 5, print "Buzz". In any other case print the integer being tested. 
+# You can print one line only for each integer tested.
